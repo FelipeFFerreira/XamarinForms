93899e55fab61a6229e001ec296c4d6944fb8d24
Felipe Ferreira <felipe@verion.ind.br>
2020-08-26 00:17:59 -0300

    Tipos de Layouts

---
+++ Secao07/App01_LayoutXF/App01_LayoutXF/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App01_LayoutXF
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void GoPaginaStack(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new Stack.StackPage());
+        }
+
+        private void GoPaginaGrid(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new Grid.GridPage());
+        }
+        private void GoPaginaAbsolute(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new Absolute.AbsolutePage());
+        }
+        private void GoPaginaRelative(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new Absolute.AbsolutePage());
+        }
+        private void GoPaginaScroll(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new Scroll.ScrollPage());
+        }
+    }
+}
